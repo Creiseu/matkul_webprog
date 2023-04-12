d419ed797ea6dd9745229933311fbcd32b3a904d
Ud <93574282+Creiseu@users.noreply.github.com>
2023-04-12 08:35:17 +0700

    new

--- latihan02/admin/mod_blog/form.php
+++ latihan02/admin/mod_blog/form.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-
-</head>
-<body>
 <form action="#" method="post">
   <div>
     <div class="mb-3 row">
@@ -44,12 +31,12 @@
         </div>
         <div class="mb-3 row">
           <div class="col-md-2">
-            ISi :
+            Isi :
           </div>
           <div class="col-md">
           <div class="form-floating">
             <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 300px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
+            <label for="floatingTextarea2">Komen</label>
           </div>
           </div>
         </div>
@@ -81,6 +68,3 @@
     </div>
   </div>
 </form>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
--- latihan02/admin/mod_kategori/index.php
+++ latihan02/admin/mod_kategori/index.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th scope="col">Id Kategori</th>
-                <th scope="col">Nama Kategori</th>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Password</th>
                 <th scope="col">Action</th>
             </tr>
         </thead>
@@ -36,5 +25,3 @@
             </tr>
         </tbody>
     </table>
-</body>
-</html>
