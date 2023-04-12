ccb2d7c98b583df8739333b376a42efc86a21755
Ud <93574282+Creiseu@users.noreply.github.com>
2023-04-12 08:40:36 +0700

    new

--- latihan02/admin/mod_kategori/index.php
+++ latihan02/admin/mod_kategori/index.php
@@ -1,27 +1,52 @@
-<table class="table table-bordered">
-    <thead>
-        <tr>
-                <th scope="col">Username</th>
-                <th scope="col">Password</th>
+
+<form action="#" method="get">
+  <section class="container-fluid">
+    <div class="mb-3 row" style="margin-top: 30px;">
+      <div class="col-md-2"></div>
+      <div class="col-md">
+        <div class="mb-3 row">
+          <div class="col-md">
+            <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btnradio3">Tambah Data</label>
+          </div>          
+        </div>
+        <div class="mb-3 row">
+          <div class="col-md">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">ID Kategori</th>
+                <th scope="col">Nama Kategori</th>
                 <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>2</td>
-                <td>Otto</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mbolang</td>
                 <td>
-                    <button type="button" class="btn btn-primary"><i class="bi bi-pencil"></i> Edit</button>
-                    <button type="button" class="btn btn-primary"><i class="bi bi-trash"></i> Delete</button>
+                  <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-check2-square"></i>Edit</label>
+                  <input type="reset" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-trash"></i>Delete</label>
                 </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Thornton</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Kuliah</td>
                 <td>
-                    <button type="button" class="btn btn-primary"><i class="bi bi-pencil"></i> Edit</button>
-                    <button type="button" class="btn btn-primary"><i class="bi bi-trash"></i> Delete</button>
+                  <input type="submit" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-check2-square"></i>Edit</label>
+                  <input type="reset" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-trash"></i>Delete</label>
                 </td>
-            </tr>
-        </tbody>
-    </table>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+  </section>
+</form>
