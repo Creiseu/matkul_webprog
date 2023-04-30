aaf52fdd0c8d376a6588769b6ac296ab60625cb2
Ud <93574282+Creiseu@users.noreply.github.com>
2023-04-30 21:52:42 +0700

    1

--- Tugas03/admin/mst_matakuliah/proses.php
+++ Tugas03/admin/mst_matakuliah/proses.php
@@ -9,7 +9,7 @@
         $kodemk = $_POST["kode_mk"];
         $namamk = $_POST["nama_mk"];
         $nilai = $_POST["nilai_mk"];
-        $exsave = mysqli_query($koneksidb, "insert into mst_matakuliah(NIM,nama,Jurusan,Kode_Matkul,Nama_Matkul,Nilai) values('.$nim.','".$nama."','".$jurusan."','.$kodemk.','".$namamk."','.$nilai.')")
+        $exsave = mysqli_query($koneksidb, "insert into mst_matakuliah(NIM,Nama,Jurusan,Kode_Matkul,Nama_Matkul,Nilai) values('.$nim.','".$nama."','".$jurusan."','.$kodemk.','".$namamk."','.$nilai.')")
         or die("gagal simpan".mysqli_error($koneksidb));
         if($exsave){
             header("Location: http://localhost/matkul_webprog/Tugas03/admin/home.php?modul=mst_matakuliah");
--- Tugas03/koneksidb.php
+++ Tugas03/koneksidb.php
@@ -1,15 +1,12 @@
 <?php
-    $servername = "localhost";
-    $database = "dblp3i";
-    $user_db = "root";
-    $pass_db = "";
+  $servernm = "localhost";
+  $database = "dblp3i";
+  $user_db = "root";
+  $pass_db = "";
 
-    $koneksidb = mysqli_connect($servername, $user_db, $pass_db, $database);
-    if(!$koneksidb){
-        echo "Koneksi Gagal";
-        exit;
-    }
-    // else{
-    //     echo "Koneksi Berhasil";
-    // }
+  $koneksidb = mysqli_connect($servernm, $user_db, $pass_db, $database);
+  if(!$koneksidb){
+      echo "Koneksi Gagal";
+      exit;
+  }
 ?>
